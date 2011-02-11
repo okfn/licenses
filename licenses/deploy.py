ebb0cddb2c45e14afc4f8610f14e2a267bf742c4
rgrp <http://rufuspollock.org>
2011-02-11 02:30:44 +0000

    [deploy,doc][s+]: refactor deploy to remove all the apache stuff and to include the new web service index page doc (www/index.html).

--- licenses/deploy.py
+++ licenses/deploy.py
@@ -1,33 +1,40 @@
 import sys
 import os
 from simplejson import dumps
+import shutil
 
-class Deploy2Command(object):
+class DeployCommand(object):
     usage = """Create a series of flat files from the licenses database that
     can directly served by a web server.
 
 Usage:
-    %s <path> <hostname>"""
+    %s <path>
+
+Deploys license files to <path>
+    """
 
     service_version = '2.0'
     group_names = ['all_alphabetical', 'ckan_original', 'ckan_canada', 'ukgov']
 
     def run(self):
-        if len(sys.argv) != 3:
+        if len(sys.argv) != 2:
             print "Usage: %s" % self.usage
             sys.exit(1)
-        self.path = os.path.join(sys.argv[1], self.service_version)
-        self.server_name = sys.argv[2]
+        self.base_path = os.path.abspath(sys.argv[1])
+        self.path = os.path.abspath(os.path.join(sys.argv[1],
+            self.service_version))
         if os.path.exists(self.path):
             print "Error: Path already exists: %s" % self.path
             sys.exit(2)
         print ""
-        print "Creating licenses service v%s in: %s" % (self.service_version, os.path.abspath(self.path))
+        print "Creating licenses service v%s in: %s" % (self.service_version, self.path)
         os.makedirs(self.path) # Will except if there isn't permission.
-        self.path = os.path.abspath(self.path)
         self.write_group_files()
-        self.write_apache_config()
-        self.write_apache_vhost()
+        indexhtml = os.path.abspath(os.path.join('.', 'licenses', 'www',
+            'index.html'))
+        indexdest = os.path.join(self.base_path, 'index.html')
+        print "Copying index.html to %s" % indexdest
+        shutil.copy(indexhtml, indexdest)
         self.print_further_instructions()
 
     def write_group_files(self):
@@ -44,50 +51,12 @@ Usage:
             file.close()
             print "'%s' has %s licenses: %s" % (group_name, len(group_licenses), file_path)
            
-    def write_apache_config(self):
-        config_content = ""
-        for group_name in self.group_names:
-            url_path = "/%s/%s" % (self.service_version, group_name)
-            group_file_path = os.path.join(self.path, group_name)
-            config_content += "Alias %s %s\n" % (url_path, group_file_path)
-        config_file_path = self.get_apache_config_file_path()
-        print ""
-        print "Writing Apache config file: %s" % config_file_path
-        config_file = open(config_file_path, 'w')
-        config_file.write(config_content)
-        config_file.close()
-
-    def get_apache_config_file_path(self):
-        return os.path.join(self.path, 'licenses-httpd.conf')
-
-    def get_apache_vhost_file_path(self):
-        return os.path.join(self.path, '%s-vhost.conf' % self.server_name)
-
-    def write_apache_vhost(self):
-        vhost_file_path = self.get_apache_vhost_file_path()
-        print "Writing example Apache VirtualHost file: %s" % vhost_file_path
-        vhost_template = """
-<VirtualHost *:80>
-    ServerName %s
-    Include %s
-</VirtualHost>
-        """
-        config_file_path = self.get_apache_config_file_path()
-        vhost_config = vhost_template % (self.server_name, config_file_path) 
-        vhost_file = open(vhost_file_path, 'w')
-        vhost_file.write(vhost_config)
-        vhost_file.close()
-
     def print_further_instructions(self):
         print ""
-        print "Finish by enabling the example Apache virtual host (or make your own)."
+        print "You now should configure your web server to serve the files at"
         print ""
-        print "Then restart Apache and check the service is working:"
-        for group_name in self.group_names:
-            print "  http://%s/%s/%s" % (self.server_name, self.service_version, group_name)
-        print ""
-
+        print "     %s" % self.base_path
 
 if __name__ == "__main__":
-    Deploy2Command().run()
+    DeployCommand().run()
 
