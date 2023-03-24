4996dbaf162527df1f651781ff9f4c1b46e5f626
Mike Linksvayer <ml@gondwanaland.com>
2023-03-24 03:50:29 +0000

    python bin/deploy.py run

---
+++ licenses/jsonp/CC-BY-ND-4.0.js
@@ -0,0 +1,14 @@
+license_callback({
+  "domain_content": true, 
+  "domain_data": true, 
+  "domain_software": false, 
+  "family": "", 
+  "id": "CC-BY-ND-4.0", 
+  "od_conformance": "approved", 
+  "osd_conformance": "not reviewed", 
+  "maintainer": "Creative Commons", 
+  "status": "active", 
+  "title": "Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0) ", 
+  "url": "https://creativecommons.org/licenses/by-nd/4.0/"
+}
+);
--- licenses/jsonp/od_plus.js
+++ licenses/jsonp/od_plus.js
@@ -38,6 +38,19 @@ license_callback({
     "title": "Creative Commons Attribution-NonCommercial 4.0",
     "url": "https://creativecommons.org/licenses/by-nc/4.0/"
   },
+  "CC-BY-ND-4.0": {
+    "domain_content": true,
+    "domain_data": true,
+    "domain_software": false,
+    "family": "",
+    "id": "CC-BY-ND-4.0",
+    "maintainer": "Creative Commons",
+    "od_conformance": "approved",
+    "osd_conformance": "not reviewed",
+    "status": "active",
+    "title": "Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0) ",
+    "url": "https://creativecommons.org/licenses/by-nd/4.0/"
+  },
   "CC-BY-SA-4.0": {
     "domain_content": true,
     "domain_data": true,
